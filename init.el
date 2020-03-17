f6a28b52141574ab5e707d2b5efe372a8171fcb3
Gökmen Görgen <gkmngrgn@gmail.com>
2020-03-17 15:24:56 +0100

    remap forward/backward paragraph shortcuts.

--- init.el
+++ init.el
@@ -71,6 +71,8 @@
 
 (global-set-key (kbd "C-c SPC") 'comment-or-uncomment-region)
 (global-set-key (kbd "C-x C-b") 'ibuffer)
+(global-set-key (kbd "M-p") 'backward-paragraph)
+(global-set-key (kbd "M-n") 'forward-paragraph)
 
 ;; GUI settings
 (if (display-graphic-p)
