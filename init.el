f79ab2086a5e755a29ff9583f34e38bdc03cddbd
Gökmen Görgen <goekmen.goergen@mimi.io>
2022-04-03 23:31:19 +0200

    add mouse support for emacs.

--- init.el
+++ init.el
@@ -48,6 +48,12 @@
 
 ;; PACKAGES
 
+;; mouse
+(unless (display-graphic-p)
+  (xterm-mouse-mode 1)
+  (global-set-key (kbd "<mouse-4>") 'scroll-down-line)
+  (global-set-key (kbd "<mouse-5>") 'scroll-up-line))
+
 ;; theme
 (straight-use-package 'modus-themes)
 (setq modus-themes-slanted-constructs t)
