58214bc96a081c1261bd309c2f07ae9f50d205e0
Gökmen Görgen <goekmen.goergen@mimi.io>
2022-05-31 13:05:56 +0300

    remove ivy, swiper, add ctrlf.

--- init.el
+++ init.el
@@ -120,7 +120,6 @@
 (add-to-list 'sml/hidden-modes " ElDoc")
 (add-to-list 'sml/hidden-modes " Golden")
 (add-to-list 'sml/hidden-modes " company")
-(add-to-list 'sml/hidden-modes " ivy")
 (add-to-list 'sml/hidden-modes " tree-sitter")
 (add-to-list 'sml/replacer-regexp-list '("^~/Workspace/" ":WS:") t)
 
@@ -153,30 +152,18 @@
 (global-flycheck-mode)
 
 ;; command completion
-(straight-use-package 'ivy)
 (straight-use-package 'counsel)
-(straight-use-package 'swiper)
 (straight-use-package 'which-key)
+(straight-use-package 'ctrlf)
 
-(ivy-mode)
 (which-key-mode)
+(ctrlf-mode +1)
 
-(setq ivy-use-virtual-buffers      t)
 (setq enable-recursive-minibuffers nil)
-(setq search-default-mode          #'char-fold-to-regexp)
 
-(global-set-key "\C-s"          'swiper)
-(global-set-key (kbd "C-c C-r") 'ivy-resume)
-(global-set-key (kbd "<f6>")    'ivy-resume)
 (global-set-key (kbd "M-x")     'counsel-M-x)
 (global-set-key (kbd "C-x b")   'counsel-switch-buffer)
 (global-set-key (kbd "C-x C-f") 'counsel-find-file)
-(global-set-key (kbd "<f1> f")  'counsel-describe-function)
-(global-set-key (kbd "<f1> v")  'counsel-describe-variable)
-(global-set-key (kbd "<f1> o")  'counsel-describe-symbol)
-(global-set-key (kbd "<f1> l")  'counsel-find-library)
-(global-set-key (kbd "<f2> i")  'counsel-info-lookup-symbol)
-(global-set-key (kbd "<f2> u")  'counsel-unicode-char)
 (global-set-key (kbd "C-c e")   'counsel-flycheck)
 (global-set-key (kbd "C-c g")   'counsel-git)
 (global-set-key (kbd "C-c j")   'counsel-git-grep)
@@ -187,7 +174,6 @@
 ;; lsp
 (straight-use-package 'lsp-mode)
 (straight-use-package 'lsp-ui)
-(straight-use-package 'lsp-ivy)
 (straight-use-package 'lsp-pyright)
 (straight-use-package 'lsp-treemacs)
 (straight-use-package 'dap-mode)
