d49361d88be5768e784674d4b634d0e71419dc33
Gökmen Görgen <gkmngrgn@gmail.com>
2023-04-26 00:02:13 +0300

    set package archives manually.

--- init.el
+++ init.el
@@ -14,10 +14,12 @@
 ;; INITIALIZE
 (require 'package)
 
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives '( "jcs-elpa" . "https://jcs-emacs.github.io/jcs-elpa/packages/") t)
-
-(setq package-archive-priorities '(("melpa" . 5) ("jcs-elpa" . 0)))
+(setq package-archives '(("gnu-elpa" . "https://elpa.gnu.org/packages/")
+                         ("jcs-elpa" . "https://jcs-emacs.github.io/jcs-elpa/packages/")
+                         ("melpa"    . "https://melpa.org/packages/"))
+      package-archive-priorities '(("gnu-elpa" . 0)
+                                   ("jcs-elpa" . 5)
+                                   ("melpa"    . 10)))
 
 (package-initialize)
 
