50e8c81b2a2c3a52496321f9e5b43f948cc121a8
Gökmen Görgen <gkmngrgn@gmail.com>
2023-05-02 00:48:36 +0300

    add marginalia, embark, embark-consult.

--- init.el
+++ init.el
@@ -76,6 +76,28 @@
   :hook (completion-list-mode . consult-preview-at-point-mode)
   :custom (consult-find-args "find . -not ( -wholename */.* -prune -o -name node_modules -prune )"))
 
+(use-package marginalia
+  :ensure t
+  :config (marginalia-mode))
+
+(use-package embark
+  :ensure t
+  :bind (("C-."   . embark-act)
+         ("C-;"   . embark-dwim)
+         ("C-h B" . embark-bindings))
+  :init
+  (setq prefix-help-command #'embark-prefix-help-command)
+  (add-hook 'eldoc-documentation-functions #'embark-eldoc-first-target)
+  :config
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none)))))
+
+(use-package embark-consult
+  :ensure t
+  :hook (embark-collect-mode . consult-preview-at-point-mode))
+
 (use-package orderless
   :ensure t
   :custom ((completion-styles             '(orderless basic))
