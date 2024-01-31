db0ebede63f864d8f24130ca753e31e103db3389
Gökmen Görgen <gkmngrgn@gmail.com>
2024-01-31 04:04:58 +0100

    fix format.

--- init.el
+++ init.el
@@ -133,9 +133,9 @@
   :ensure t
   :hook (prog-mode . copilot-mode)
   :bind (:map copilot-completion-map
-              ("<tab>" . 'copilot-accept-completion)
-              ("TAB" . 'copilot-accept-completion)
-              ("C-TAB" . 'copilot-accept-completion-by-word)
+              ("<tab>"   . 'copilot-accept-completion)
+              ("TAB"     . 'copilot-accept-completion)
+              ("C-TAB"   . 'copilot-accept-completion-by-word)
               ("C-<tab>" . 'copilot-accept-completion-by-word)))
 
 ;;; init.el ends here
