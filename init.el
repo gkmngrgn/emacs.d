b5af40bb1f76e48991fd801239369a5a963db75a
Gökmen Görgen <gkmngrgn@gmail.com>
2024-04-09 00:20:36 +0200

    remove embark. I don't need it.

--- init.el
+++ init.el
@@ -78,23 +78,6 @@
   :ensure t
   :config (marginalia-mode))
 
-(use-package embark
-  :ensure t
-  :bind (("C-."   . embark-act)
-         ("C-;"   . embark-dwim)
-         ("C-h B" . embark-bindings))
-  :init
-  (setq prefix-help-command #'embark-prefix-help-command)
-  :config
-  (add-to-list 'display-buffer-alist
-               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
-                 nil
-                 (window-parameters (mode-line-format . none)))))
-
-(use-package embark-consult
-  :ensure t
-  :hook (embark-collect-mode . consult-preview-at-point-mode))
-
 (use-package orderless
   :ensure t
   :custom ((completion-styles             '(orderless basic))
